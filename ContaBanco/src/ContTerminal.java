611cdca5561d94499c76e1e39d74379e79688acd
Daniel Souza <danielrsouza10@gmail.com>
2023-07-04 14:55:41 -0300

    comentários adicionados

--- ContaBanco/src/ContTerminal.java
+++ ContaBanco/src/ContTerminal.java
@@ -1,12 +1,14 @@
 import java.util.Locale;
+//importar a classe scanner
 import java.util.Scanner;
 
 public class ContTerminal {
     
     public static void main(String[] args) {
-       
+        //criar a classe scanner
         Scanner conta = new Scanner(System.in).useLocale(Locale.US);
 
+        //solicitar o input do usuário
         System.out.print("Digite o seu nome: ");
         String nomeCliente = conta.next();
 
@@ -19,6 +21,7 @@ public class ContTerminal {
         System.out.print("Digite o saldo da conta: ");
         double saldoConta = conta.nextDouble();
 
+        //apresentação das informações inseridas pelo usuário
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque.");
 
 
